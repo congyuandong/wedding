7783c9d77ab4442872075c744ac7156f9c73252b
congyuandong <congyuandong@gmail.com>
2017-12-06 01:03:41 +0800

    update

--- config/weixin.js
+++ config/weixin.js
@@ -2,5 +2,5 @@ module.exports.weixin = {
   // 微信公众号相关设置
   appid: 'wx7a523bb86272a83b',
   secret: '507a77a32430389fbf8a8f58db09468f',
-  redirectUri: 'http://116.62.151.42:8000/home/resolve'
+  redirectUri: 'http://love.congyuandong.cn/home/resolve'
 };
