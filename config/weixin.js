b60c278de0b4061f3e4174eb1043fa6ebbb2952a
congyuandong <congyuandong@gmail.com>
2017-12-06 00:54:20 +0800

    update: update wechat url

--- config/weixin.js
+++ config/weixin.js
@@ -2,5 +2,5 @@ module.exports.weixin = {
   // 微信公众号相关设置
   appid: 'wx7a523bb86272a83b',
   secret: '507a77a32430389fbf8a8f58db09468f',
-  redirectUri: 'http://127.0.0.1:1337/home/resolve'
+  redirectUri: 'http://love.congyuandong.cn/home/resolve'
 };
