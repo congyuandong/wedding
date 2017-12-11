a4099967f69c0f55cdf400868e7de642a5e2f86c
scott <congyuandong@gmail.com>
2017-12-11 12:43:19 +0800

    Update pm.js

--- assets/js/pm.js
+++ assets/js/pm.js
@@ -370,7 +370,7 @@
         }).done(function() {
           var map = new AMap.Map('pnl-map',{
             zoom: 17,
-            center: [115.977634, 29.709759]
+            center: [120.945737, 32.399224]
           });
           var marker = new AMap.Marker({
             position: map.getCenter()
@@ -380,7 +380,7 @@
           // 设置label标签
           marker.setLabel({
             offset: new AMap.Pixel(-75, -30),
-            content: "PM Infinity婚礼于10月5日晚"
+            content: "婚礼于12月11日晚在水一方大酒店"
           });
         }).fail(function() {
 
